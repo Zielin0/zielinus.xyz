2ac3379245769aa00d574fa0b77abf1e2d971303
Zielin0 <me@zielinus.xyz>
2023-01-10 21:04:26 +0100

    chore(lint): add pretteir & eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-airbnb-with-typescript',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'vuejs-accessibility/role-has-required-aria-props': 0,
+  },
+};
