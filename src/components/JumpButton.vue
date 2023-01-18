3b82b93fb0dfff31ebba75a0efef72b9a1a30d26
Zielin0 <me@zielinus.xyz>
2023-01-18 19:21:02 +0100

    feat(button): add button for hero section

---
+++ src/components/JumpButton.vue
@@ -0,0 +1,14 @@
+<!-- @TODO: Add option for only border button for 'About Me' -->
+
+<script lang="ts" setup>
+defineProps(['targetId']);
+</script>
+
+<template>
+  <a
+    class="bg-gradient-to-r from-pink to-purple rounded px-5 py-3 text-2xl text-center"
+    :href="targetId"
+  >
+    <slot />
+  </a>
+</template>
--- src/components/section/HeroSection.vue
+++ src/components/section/HeroSection.vue
@@ -1,10 +1,18 @@
 <script lang="ts" setup>
 import ColoredText from '../ColoredText.vue';
+import JumpButton from '../JumpButton.vue';
 </script>
 
 <template>
   <div>
     <h1 class="text-5xl">Hello! I am <ColoredText>Zielino</ColoredText></h1>
     <h1 class="text-white-second text-2xl mt-7">Welcome on my website.</h1>
+
+    <div class="mt-16">
+      <JumpButton class="mr-[13vmin]" target-id="#projects">
+        Projects
+      </JumpButton>
+      <JumpButton target-id="#about">About Me</JumpButton>
+    </div>
   </div>
 </template>
