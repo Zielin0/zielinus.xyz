efd7ab9b4f1e5cbc4c00a9cc79b367c527d23de8
Zielin0 <me@zielinus.xyz>
2023-01-19 18:55:41 +0100

    chore(button): add animation on hover

--- src/components/JumpButton.vue
+++ src/components/JumpButton.vue
@@ -11,14 +11,14 @@ defineProps({
 <template>
   <a
     v-if="!secondary"
-    class="bg-gradient-to-r from-pink to-purple rounded px-5 py-3 text-2xl text-center text-pure-white"
+    class="bg-gradient-to-r from-pink to-purple rounded px-5 py-3 text-2xl text-center text-pure-white bg-[length:200%] bg-[height:100%] hover:animate-horizontal"
     :href="targetId"
   >
     <slot />
   </a>
   <a
     v-else
-    class="bg-gradient-to-r from-pink to-purple rounded px-1 py-3 text-2xl text-center text-pure-white"
+    class="bg-gradient-to-r from-pink to-purple rounded px-1 py-3 text-2xl text-center text-pure-white bg-[length:200%] bg-[height:100%] hover:animate-horizontal"
     :href="targetId"
   >
     <span class="bg-light-gray rounded px-3 py-2">
