c21695cfff81d4e14f9b2486b448b2120251bcf8
Zielin0 <me@zielinus.xyz>
2023-01-23 18:25:38 +0100

    feat(sections): add FooterSection with social links and copyright mark

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import AboutSection from '../section/AboutSection.vue';
+import FooterSection from '../section/FooterSection.vue';
 import HeroSection from '../section/HeroSection.vue';
 import ProjectSection from '../section/ProjectSection.vue';
 </script>
@@ -13,5 +14,15 @@ import ProjectSection from '../section/ProjectSection.vue';
   </div>
   <div class="font-rowdies flex justify-center text-center mt-20 text-white">
     <AboutSection id="about" />
+  </div>
+  <div class="font-rowdies flex justify-center text-center mt-20 text-white">
+    <FooterSection />
+  </div>
+  <div
+    class="font-rowdies font-normal flex justify-center text-center mt-10 mb-5"
+  >
+    <h1 class="text-comment text-md lg:text-xl blur-[1px]">
+      Zielino &copy; 2023
+    </h1>
   </div>
 </template>
---
+++ src/components/section/FooterSection.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import LinkComponent from '../LinkComponent.vue';
+
+const socialList = ref([
+  {
+    name: 'mail',
+    icon: 'fa-solid fa-at hover:text-[#FFFFFF]',
+    link: 'mailto:me@zielinus.xyz',
+  },
+  {
+    name: 'github',
+    icon: 'fa-brands fa-github hover:text-[#DBDBDB]',
+    link: 'https://github.com/Zielin0',
+  },
+  {
+    name: 'discord',
+    icon: 'fa-brands fa-discord hover:text-[#5865F2]',
+    link: 'https://discord.com/invite/yjVgBQtJ2J',
+  },
+  {
+    name: 'youtube',
+    icon: 'fa-brands fa-youtube hover:text-[#E52D27]',
+    link: 'https://youtube.com/@ZielinoCoding',
+  },
+  {
+    name: 'twitch',
+    icon: 'fa-brands fa-twitch hover:text-[#6441A4]',
+    link: 'https://twitch.tv/zielin0',
+  },
+  {
+    name: 'twitter',
+    icon: 'fa-brands fa-twitter hover:text-[#00ACEE]',
+    link: 'https://twitter.com/theZielino',
+  },
+]);
+</script>
+
+<template>
+  <div
+    v-for="social of socialList"
+    v-bind:key="social.name"
+    class="text-white-gray text-3xl lg:text-4xl px-2 lg:px-3"
+  >
+    <LinkComponent :href="social.link">
+      <i class="transition-colors" :class="social.icon" />
+    </LinkComponent>
+  </div>
+</template>
